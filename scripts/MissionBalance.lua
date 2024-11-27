eef5f27155bfa0e2999610b1bacf77a00e244639
emwedk <emwedk@gmail.com>
2024-11-27 18:52:20 +0100

    resolve merge

--- scripts/MissionBalance.lua
+++ scripts/MissionBalance.lua
@@ -21,35 +21,46 @@ function MissionBalance:scaleMissionReward()
 
     local rewardFactor = ContractBoost.config.rewardFactor
     
-    for _, missionType in ipairs(g_missionManager.missionTypes) do
-        
-        local typeName = missionType.name
-        local prevValue = nil
-        local newValue = nil
+    if #g_missionManager.missionTypes then
+        for _, missionType in ipairs(g_missionManager.missionTypes) do
+            
+            local typeName = missionType.name
+            local prevValue = nil
+            local newValue = nil
 
-        if typeName == "baleWrapMission" then
-            prevValue = missionType.data.rewardPerBale
-            newValue = ContractBoost.config.customRewards[typeName] or missionType.data.rewardPerBale * rewardFactor
-            missionType.data.rewardPerBale = newValue
-        elseif typeName == "deadwoodMission" or typeName == "treeTransportMission" then
-            prevValue = missionType.data.rewardPerTree
-            newValue = ContractBoost.config.customRewards[typeName] or missionType.data.rewardPerTree * rewardFactor
-            missionType.data.rewardPerTree = newValue
-        elseif typeName == "destructibleRockMission" then
-            prevValue = missionType.data.rewardPerRock
-            newValue = ContractBoost.config.customRewards[typeName] or missionType.data.rewardPerRock * rewardFactor
-            missionType.data.rewardPerRock = newValue
-        else
-            prevValue = missionType.data.rewardPerHa
-            newValue = ContractBoost.config.customRewards[typeName] or missionType.data.rewardPerHa * rewardFactor
-            missionType.data.rewardPerHa = newValue
+            -- if ContractBoost.debug then
+            --     printf('---- ContractBoost:MissionBalance :: %s data', typeName)
+            --     DebugUtil.printTableRecursively(missionType.data)
+            -- end
+
+            -- don't process the contract type if there are no instances
+            if missionType.data.numInstances > 0 then
+
+                if typeName == "baleWrapMission" then
+                    prevValue = missionType.data.rewardPerBale
+                    newValue = ContractBoost.config.customRewards[typeName] or missionType.data.rewardPerBale * rewardFactor
+                    missionType.data.rewardPerBale = newValue
+                elseif typeName == "deadwoodMission" or typeName == "treeTransportMission" then
+                    prevValue = missionType.data.rewardPerTree
+                    newValue = ContractBoost.config.customRewards[typeName] or missionType.data.rewardPerTree * rewardFactor
+                    missionType.data.rewardPerTree = newValue
+                elseif typeName == "destructibleRockMission" then
+                    prevValue = missionType.data.rewardPerRock
+                    newValue = ContractBoost.config.customRewards[typeName] or missionType.data.rewardPerRock * rewardFactor
+                    missionType.data.rewardPerRock = newValue
+                else
+                    prevValue = missionType.data.rewardPerHa
+                    newValue = ContractBoost.config.customRewards[typeName] or missionType.data.rewardPerHa * rewardFactor
+                    missionType.data.rewardPerHa = newValue
+                end
+
+                -- update the maximum number of each type to it's custom value if it exists else use the default
+                missionType.data.maxNumInstances = ContractBoost.config.customMaxPerType[typeName] or ContractBoost.config.maxContractsPerType
+
+                if ContractBoost.debug then printf('---- ContractBoost:MissionBalance :: Mission %s: %s | updated %s => %s', missionType.typeId, missionType.name, prevValue, newValue) end
+            end
         end
 
-        -- update the maximum number of each type to it's custom value if it exists else use the default
-        missionType.data.maxNumInstances = ContractBoost.config.customMaxPerType[typeName] or ContractBoost.config.maxContractsPerType
-
-        if ContractBoost.debug then printf('---- ContractBoost:MissionBalance :: Mission %s: %s | updated %s => %s', missionType.typeId, missionType.name, prevValue, newValue) end
+        if ContractBoost.debug then print('-- ContractBoost:MissionBalance :: scaleMissionReward complete.') end
     end
-
-    if ContractBoost.debug then print('-- ContractBoost:MissionBalance :: scaleMissionReward complete.') end
 end
